94219c2dcc5d82d9fb6dfa32295f4cdc0eed4761
Ridan77 <dan.ribak17@gmail.com>
2025-08-27 23:09:22 +0300

    start user page

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -7,6 +7,7 @@ import { loadStories } from '../store/actions/story.actions'
 import { store } from '../store/store'
 import { showSuccessMsg } from '../services/event-bus.service'
 import { socketService, SOCKET_EVENT_USER_UPDATED, SOCKET_EMIT_USER_WATCH } from '../services/socket.service'
+import { svg } from '../cmps/Svgs'
 
 export function UserDetails() {
 
@@ -33,19 +34,20 @@ export function UserDetails() {
     showSuccessMsg(`This user ${user.fullname} just got updated from socket, new score: ${user.score}`)
     store.dispatch({ type: 'SET_WATCHED_USER', user })
   }
-
+console.log(stories);
+if (!user) return <div>Wait</div>
   return (
     <section className="user-details">
-      <h1>User Details</h1>
-      {user && <div>
-        <h3>
-          {user.fullname}
-        </h3>
-        <img src={user.imgUrl} style={{ width: '100px' }} />
-        <pre> {JSON.stringify(user, null, 2)} </pre>
-        <hr />
-        <pre>{JSON.stringify(stories)}</pre>
-      </div>}
+      <section className='user-header'>
+        <img src={user.imgUrl} alt="" />
+        <span>{user.fullname}</span>
+        <button>Follow</button>
+        <span>{svg.more}</span>
+        <span>{`${stories?.length} Posts`}</span>
+        <span>{`${user.followers} Followers`}</span>
+        <span>{`${user.following} Following`}</span>
+        <span>{user.bio}</span>
+      </section>
     </section>
   )
 }
