88deee29cca45f8f18db00bcdf717d7c8915bdd1
Ridan77 <dan.ribak17@gmail.com>
2025-09-16 12:27:58 +0300

    fix user details responsivness

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -1,78 +1,81 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { Link, useParams } from "react-router-dom";
+import { useEffect } from "react"
+import { useSelector } from "react-redux"
+import { Link, useParams } from "react-router-dom"
 
-import { loadUser, toggleFollow } from "../store/actions/user.actions";
-import { loadStories } from "../store/actions/story.actions";
-import { store } from "../store/store";
-import { showSuccessMsg } from "../services/event-bus.service";
+import { loadUser, toggleFollow } from "../store/actions/user.actions"
+import { loadStories } from "../store/actions/story.actions"
+import { store } from "../store/store"
+import { showSuccessMsg } from "../services/event-bus.service"
 import {
   socketService,
   SOCKET_EVENT_USER_UPDATED,
   SOCKET_EMIT_USER_WATCH,
-} from "../services/socket.service";
-import { svg } from "../cmps/Svgs";
+} from "../services/socket.service"
+import { svg } from "../cmps/Svgs"
 import { Gallery } from "../cmps/Gallery"
 import { Loader } from "../cmps/Loader"
 
 export function UserDetails() {
-  const params = useParams();
-  const user = useSelector((storeState) => storeState.userModule.watchedUser);
-  const stories = useSelector((storeState) => storeState.storyModule.stories);
-  const loggedinUser = useSelector((storeState) => storeState.userModule.user);
-  
-  useEffect(() => {
-    loadUser(params.id);
-    loadStories({ userId: params.id,random:true });
+  const params = useParams()
+  const user = useSelector((storeState) => storeState.userModule.watchedUser)
+  const stories = useSelector((storeState) => storeState.storyModule.stories)
+  const loggedinUser = useSelector((storeState) => storeState.userModule.user)
 
-    socketService.emit(SOCKET_EMIT_USER_WATCH, params.id);
-    socketService.on(SOCKET_EVENT_USER_UPDATED, onUserUpdate);
+  useEffect(() => {
+    loadUser(params.id)
+    loadStories({ userId: params.id, random: true })
+
+    socketService.emit(SOCKET_EMIT_USER_WATCH, params.id)
+    socketService.on(SOCKET_EVENT_USER_UPDATED, onUserUpdate)
 
     return () => {
-      socketService.off(SOCKET_EVENT_USER_UPDATED, onUserUpdate);
-    };
-  }, [params.id]);
+      socketService.off(SOCKET_EVENT_USER_UPDATED, onUserUpdate)
+    }
+  }, [params.id])
 
   async function onToggleFollow(userToFollowId) {
     try {
-      await toggleFollow(userToFollowId);
+      await toggleFollow(userToFollowId)
     } catch (err) {
-      console.log("error", err);
+      console.log("error", err)
     }
   }
 
   function onUserUpdate(user) {
     showSuccessMsg(
       `This user ${user.fullname} just got updated from socket, new score: ${user.score}`
-    );
-    store.dispatch({ type: "SET_WATCHED_USER", user });
+    )
+    store.dispatch({ type: "SET_WATCHED_USER", user })
   }
-  if (!user) return <Loader/>;
+  if (!user) return <Loader />
   return (
     <section className="user-details">
       <section className="user-header">
         <img src={user.imgUrl} className="user-img" alt="" />
-        <div className="name-section">
-          <span>{user.fullname}</span>
-          <button
-            onClick={() => onToggleFollow(user._id)}
-            className="follow-btn">
-            {user.followers?.some(item=>item._id===loggedinUser._id) ? 'Unfollow' : 'Follow'}
-          </button>
-          <span className="more">{svg.more}</span>
-        </div>
-        <div className="user-counters">
-          <span className="bold">{stories?.length} </span>
-          <span className="gray">Posts</span>
-          <span className="bold">{user.followers?.length}</span>
-          <span className="gray">Followers</span>
-          <span className="bold">{user.following?.length}</span>
-          <span className="gray"> Following</span>
-        </div>
-      </section>
+        <section className="sub-header">
+          <div className="name-section">
+            <span>{user.fullname}</span>
+            <button
+              onClick={() => onToggleFollow(user._id)}
+              className="follow-btn">
+              {user.followers?.some((item) => item._id === loggedinUser._id)
+                ? "Unfollow"
+                : "Follow"}
+            </button>
+            <span className="more">{svg.more}</span>
+          </div>
+          <div className="user-counters">
+            <span className="bold no1">{stories?.length}</span>
+            <span className="gray no2">Posts</span>
+            <span className="bold no3">{user.followers?.length}</span>
+            <span className="gray no4">Followers</span>
+            <span className="bold no5">{user.following?.length}</span>
+            <span className="gray no6"> Following</span>
+          </div>
+        </section>
         {/* <span>{user.bio}</span> */}
-      <Gallery stories={stories}/>
-      <Loader/>
+      </section>
+      <Gallery stories={stories} />
     </section>
-  );
+  )
 }
